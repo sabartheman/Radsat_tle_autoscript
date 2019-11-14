1c5e541fc7aea037d13f2d7974e8690449c5be41
skylart <skylar4494@gmail.com>
2019-11-14 12:43:06 -0700

    updating format to be user friendly

--- pullradsattle.py
+++ pullradsattle.py
@@ -4,20 +4,35 @@ from spacetrack import SpaceTrackClient
 import spacetrack.operators as op
 import datetime
 
-#account info for space-track.org
-#free account with minimal verification
-st = SpaceTrackClient('username', 'password')
 
-#list of the cubesats wanted
-cubesats = [225544, 43546,43547,43548,43549,43550,43551,43552,43553,43554]
-
-#printing off the list of values that will be pulled from the tle database
-
-#lines for a file
-lines = st.tle_latest(norad_cat_id=cubesats,ordinal=1,format='3le')
-print(lines)
+def pullTLEs(stuser, stpass, cubesats):
+    #account info for space-track.org
+    #free account with minimal verification
+    st = SpaceTrackClient(stuser, stpass)
 
 
-with open('tle.txt', 'w') as fp:
-    for line in lines:
-        fp.write(line)
+    #list of the cubesats wanted
+    # cubesats = [43377, 43378, 43553]
+    #printing off the list of values that will be pulled from the tle database
+
+    #lines for a file
+    lines = st.tle_latest(norad_cat_id=cubesats,ordinal=1,format='3le')
+    print(lines)
+
+
+    with open('tle.txt', 'w') as fp:
+        for line in lines:
+            fp.write(line)
+
+
+
+
+if __name__ == "__main__":
+
+    stuser = "exampleEmail@sample.org"
+    stpass = "examplePassword"
+
+    #These are your NoradIDs you're looking for.
+    cubesats = [40377, 40378, 43553]
+
+    pullTLEs(stuser, stpass, cubesats)
